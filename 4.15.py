40dcd150742aad8647636adcc3f97e9e51d436a8
nguyenvannhat582 <48303068+nguyenvannhat582@users.noreply.github.com>
2019-04-04 21:23:13 +0700

    tb
    tb

---
+++ 4.15.py
@@ -0,0 +1,3 @@
+items=[x for x in input("nhap mot chuoi:").split(',')]
+items.sort()
+print(','.join(items))
---
+++ 4.23.py
@@ -0,0 +1,7 @@
+n=input("nhap chuoi:")
+dict={'H':0,'T':0}
+for i in n:
+    if i.isupper():
+        dict{'H'}=dict{'H'}
+        dict{'T'}+=1.
+ print(dict)
