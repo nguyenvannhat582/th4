42393da0723da8984f720026c23826b9cd5ba867
nguyenvannhat582 <48303068+nguyenvannhat582@users.noreply.github.com>
2019-04-25 15:53:52 +0700

    Add files via upload

---
+++ 4.3.py
@@ -0,0 +1,3 @@
+s=input('nhap chuoi:').upper()
+for ch in s:
+    print(ch)
---
+++ 4.5.py
@@ -0,0 +1,7 @@
+ds=input('danh sach:').split()
+ds.reverse()
+    #in cả dãy vừa nhập
+print(ds)
+    #in dãy vừa nhập,mỗi phần tử trên một dòng
+for so in ds:
+    print(so)
